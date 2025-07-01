4f6a58f1fc86616131e8908bc6fbeb7875e586b6
Ian Alexis Solis Hernández <ianalexis9137@gmail.com>
2025-06-30 23:54:46 -0600

    Comentarios de secciones

--- Principal.cpp
+++ Principal.cpp
@@ -104,6 +104,7 @@ int main() {
     // Puntero al archivo
     FIL C0_file, C1_file;
 
+    //=================================================Manejadores de archivos=================================================
     LSM_handler = (SensorHandler){
         .is_connected = false,
         .filename = "LSM9DS1.csv",
@@ -168,14 +169,17 @@ int main() {
         .buffer_tail = 0,
         .buffer_full = false
     };
-    //=================================================Inicializacion imu=================================================
-    if(lsm.init_accel(
-                LSM9DS1::SCALE_GYRO_500DPS, 
-                LSM9DS1::SCALE_ACCEL_4G, 
-                LSM9DS1::ODR_119HZ, LSM9DS1::ODR_119HZ) && 
-                lsm.init_magnetometer(
-                    LSM9DS1::MAG_SCALE_4GAUSS, 
-                    LSM9DS1::MAG_ODR_80HZ)){
+    //=========================================================================================================================
+
+
+    bool success;
+
+    
+    //=================================================Inicializacion del sensor LSM9DS1=================================================
+    success = lsm.init_accel( LSM9DS1::SCALE_GYRO_500DPS,  LSM9DS1::SCALE_ACCEL_4G,  LSM9DS1::ODR_119HZ, LSM9DS1::ODR_119HZ) && 
+              lsm.init_magnetometer(LSM9DS1::MAG_SCALE_4GAUSS, LSM9DS1::MAG_ODR_80HZ);
+
+    if(success){
         lsm.calibrate_magnetometer(0.15f, 0.08f, -0.47f);
         lsm.calibrate_gyro(-0.2673f, 0.5627f, 0.8419);
         if(abrir_archivo(LSM_handler.file, LSM_handler.filename)){
@@ -193,17 +197,11 @@ int main() {
     else{
         LSM_handler.is_connected = false;
     }
-    //====================================================================================================================
+    //===================================================================================================================================
 
     //=============================================Inicializacion magnetometro y termometro=============================================
-    if(mlx.init_sensor(
-        0x8000, 0x8000, 0x8000,      // Offsets X,Y,Z
-        MLX90393::RESOLUTION_MAX,   // Resolucion X
-        MLX90393::RESOLUTION_MAX,   // Resolucion Y
-        MLX90393::RESOLUTION_MAX,   // Resolucion Z
-        MLX90393::FILT_1,           // Filtro digital
-        MLX90393::OSR_MAX         // Oversampling
-    )){
+    success = mlx.init_sensor(0x8000, 0x8000, 0x8000,MLX90393::RESOLUTION_MAX,MLX90393::RESOLUTION_MAX,MLX90393::RESOLUTION_MAX,MLX90393::FILT_1,MLX90393::OSR_MAX);
+    if(success){
         if(abrir_archivo(MLX_mag_handler.file, MLX_mag_handler.filename)){
             if (f_printf(MLX_mag_handler.file, "B(x)[uT],B(y)[uT],B(z)[uT],t[ms]\n") < 0) {
                 printf("f_printf failed\n");
@@ -234,11 +232,9 @@ int main() {
     }
     //========================================================================================================================================
     
-    if(ms5803.init_sensor(
-        MS5803::T_OSR_4096,
-        MS5803::P_OSR_4096 ,
-        MS5803::ADDRESS_HIGH
-    )){
+    //====================================================Inicializacion del sensor MS5803====================================================
+    success = ms5803.init_sensor(MS5803::T_OSR_4096, MS5803::P_OSR_4096 , MS5803::ADDRESS_HIGH);
+    if(success){
         if(abrir_archivo(MS5803_handler.file, MS5803_handler.filename)){
             if (f_printf(MS5803_handler.file, "T[C],P[mbar],t[s]\n") < 0) {
                 printf("f_printf failed\n");
@@ -254,8 +250,11 @@ int main() {
     else{
         MS5803_handler.is_connected = false;
     }
+    //========================================================================================================================================
 
-    if(veml.init_sensor(VEML6030::HIGH, VEML6030::GAIN_0_125, VEML6030::IT_100MS)){
+    //===================================================Inicializacion del sensor VEML6030===================================================
+    success = veml.init_sensor(VEML6030::HIGH, VEML6030::GAIN_0_125, VEML6030::IT_100MS);
+    if(success){
         if(abrir_archivo(VEML_handler.file, VEML_handler.filename)){
             if (f_printf(VEML_handler.file, "Ambiental[lux],Blanca[lux],t[s]\n") < 0) {
                 printf("f_printf failed\n");
@@ -270,8 +269,11 @@ int main() {
     else{
         VEML_handler.is_connected = false;
     }
+    //========================================================================================================================================
 
-    if(am23.init_sensor()){
+    //====================================================Inicializacion del sensor AM2302====================================================
+    success = am23.init_sensor();
+    if(success){
         if(abrir_archivo(AM23_handler.file, AM23_handler.filename)){
             if (f_printf(AM23_handler.file, "Humedad relativa [%%], Temperatura [C], Status ,t[s]\n") < 0) {
                 printf("f_printf failed\n");
@@ -285,8 +287,11 @@ int main() {
     }else{
         AM23_handler.is_connected = false;
     }
+    //========================================================================================================================================
 
-    if(isl.init_sensor()){
+    //===================================================Inicializacion del sensor ISL29125===================================================
+    success = isl.init_sensor(ISL29125::MODE_RGB, ISL29125::RAN_10_000 ,ISL29125::RES_16, false, ISL29125::IR_LOW);
+    if(success){
         if(abrir_archivo(ISL_handler.file, ISL_handler.filename)){
             if (f_printf(ISL_handler.file, "Rojo [lux],Verde [lux],Azul [lux],t[s]\n") < 0) {
                 printf("f_printf failed\n");
@@ -301,6 +306,7 @@ int main() {
     else{
         ISL_handler.is_connected = false;        
     }
+    //==========================================================================================================================================
 
     // Interrupcion para leer los sensores cada 10 ms
     struct repeating_timer timer_c_0;
@@ -313,7 +319,7 @@ int main() {
 
     while(1) {
 
-        // Se verifica si el buffer tiene informacion por leer
+        //=========================================================Guardar en SD LSM9DS1=========================================================
         if(esta_conectado(&LSM_handler) && buffer_tiene_elementos(&LSM_handler)){
             actualizar_buffer(&LSM_handler, LSM_handler.lsm_buffer, LSM_handler.lsm_current);
             mutex_enter_blocking(&spi_mutex);
@@ -322,7 +328,9 @@ int main() {
             }
             mutex_exit(&spi_mutex);
         }
+        //=======================================================================================================================================
 
+        //=========================================================Guardar en SD MLX90393=========================================================
         if(esta_conectado(&MLX_mag_handler) && buffer_tiene_elementos(&MLX_mag_handler)){
             actualizar_buffer(&MLX_mag_handler, MLX_mag_handler.mlx_buffer, MLX_mag_handler.mlx_current);
             mutex_enter_blocking(&spi_mutex);
@@ -331,6 +339,8 @@ int main() {
             }
             mutex_exit(&spi_mutex);
         }
+        //========================================================================================================================================
+
         // // TODO: Reintentar el montaje de la SD cada X tiempo por un par de intentos
         if (sd_card.sd_test_com && !sd_card.sd_test_com(&sd_card)) {
             printf("La tarjeta SD fue retirada, desmontando...\n");
@@ -358,7 +368,7 @@ bool capturar10ms(__unused repeating_timer *t)
     // Se guarda el tiempo transcurrido en una variable segura para evitar conflictos de acceso
 
     
-    //===================================Leer LSM9DS1===================================
+    //===================================Leer y guardar LSM9DS1===================================
     if(esta_conectado(&LSM_handler)){
         lsm.read_accelerometer();
         lsm.read_gyroscope();
@@ -368,9 +378,9 @@ bool capturar10ms(__unused repeating_timer *t)
         mutex_exit(&time_mutex);
         guardar_en_buffer(&LSM_handler, LSM_handler.lsm_buffer, lsm.last_measurement);  
     }
-    //==================================================================================
+    //===========================================================================================
 
-    //==================================Leer MLX90393 (magnetometro y termometro solo leer)==================================
+    //==================================Leer MLX90393 (guardar y leer magnetometro, termometro solo leer)==================================
     if(esta_conectado(&MLX_mag_handler)){
         if (MLX_mag_handler.flag_1) {                   // Indica que es la primera vez que se mide el magnetometro
             // 1. Comenzar una nueva medición del magnetometro
@@ -386,18 +396,19 @@ bool capturar10ms(__unused repeating_timer *t)
             mlx.last_measurement.time_ms = safe_elapsed_ms_c0;
         }
     }
-    //======================================================================================================================
+    //=====================================================================================================================================
 
     
     return true;
 }
 
 void core1_main() {
-    printf("core1_main iniciado\n");
     // Interrupcion para leer los sensores cada 10 s
     struct repeating_timer timer_c_1;
     add_repeating_timer_ms(10000, capturar10s, NULL, &timer_c_1);
     while(1){
+        //=========================================================Guardar en SD MLX90393=========================================================
+
        if(esta_conectado(&MLX_temp_handler) && buffer_tiene_elementos(&MLX_temp_handler)){
             actualizar_buffer(&MLX_temp_handler, MLX_temp_handler.mlx_buffer, MLX_temp_handler.mlx_current);
             mutex_enter_blocking(&spi_mutex);
@@ -406,6 +417,9 @@ void core1_main() {
             }
             mutex_exit(&spi_mutex);
         } 
+        //========================================================================================================================================
+
+        //=========================================================Guardar en SD MS803=========================================================
         if(esta_conectado(&MS5803_handler) && buffer_tiene_elementos(&MS5803_handler)){
             actualizar_buffer(&MS5803_handler, MS5803_handler.ms58903_buffer, MS5803_handler.ms5803_current);
             mutex_enter_blocking(&spi_mutex);
@@ -414,6 +428,9 @@ void core1_main() {
             }
             mutex_exit(&spi_mutex);
         }
+        //======================================================================================================================================
+
+        //=========================================================Guardar en SD VEML6030=========================================================
         if(esta_conectado(&VEML_handler) && buffer_tiene_elementos(&VEML_handler)){
             actualizar_buffer(&VEML_handler, VEML_handler.veml_buffer, VEML_handler.veml_current);
             mutex_enter_blocking(&spi_mutex);
@@ -422,7 +439,9 @@ void core1_main() {
             }
             mutex_exit(&spi_mutex);                
         }
+        //========================================================================================================================================
 
+        //=========================================================Guardar en SD AM2302=========================================================
         if(esta_conectado(&AM23_handler) && buffer_tiene_elementos(&AM23_handler)){
             actualizar_buffer(&AM23_handler, AM23_handler.am23_buffer, AM23_handler.am23_current);
             mutex_enter_blocking(&spi_mutex);
@@ -431,7 +450,9 @@ void core1_main() {
             }     
             mutex_exit(&spi_mutex);        
         }
+        //======================================================================================================================================
 
+        //=========================================================Guardar en SD ISL29125=========================================================
         if(esta_conectado(&ISL_handler) && buffer_tiene_elementos(&ISL_handler)){
             actualizar_buffer(&ISL_handler, ISL_handler.isl_buffer, ISL_handler.isl_current);
             mutex_enter_blocking(&spi_mutex);
@@ -440,6 +461,7 @@ void core1_main() {
             }     
             mutex_exit(&spi_mutex); 
         }
+        //=========================================================================================================================================
     }
 
 }
@@ -453,12 +475,12 @@ bool capturar10s(__unused repeating_timer *t){
     mutex_exit(&time_mutex);
 
     //=====================================Guardar MLX90393 (temperatura)=====================================
-
     if(esta_conectado(&MLX_temp_handler)){
         guardar_en_buffer(&MLX_temp_handler, MLX_temp_handler.mlx_buffer, mlx.last_measurement);
     }
     //========================================================================================================
 
+    //==========================================Leer y guardar MS5803==========================================
     if(esta_conectado(&MS5803_handler)){
         if(MS5803_handler.flag_1){                                                      // Es la primera vez que se mide cualquiera de las dos
             ms5803.start_measurement_temp();
@@ -474,14 +496,18 @@ bool capturar10s(__unused repeating_timer *t){
             add_alarm_in_ms(ms5803.aquisition_time, get_ms5803, NULL, false);      // Se lanza la alarma para guardar medicion de temperatura
         }
     }
+    //=========================================================================================================
 
+    //=========================================Leer y guardar VEML6030=========================================
     if(esta_conectado(&VEML_handler)){
         veml.read_ambient();
         veml.read_white();
         veml.last_measurement.time_ms = safe_elapsed_ms_c1;
         guardar_en_buffer(&VEML_handler, VEML_handler.veml_buffer, veml.last_measurement);
     }
+    //=========================================================================================================
 
+    //==========================================Leer y guardar AM2302==========================================
     if(esta_conectado(&AM23_handler)){
         if(AM23_handler.flag_1){
             am23.start_measurement();
@@ -494,12 +520,15 @@ bool capturar10s(__unused repeating_timer *t){
             am23.last_measurement.time_ms = safe_elapsed_ms_c1;
         }
     }
+    //=========================================================================================================
 
-        if(esta_conectado(&ISL_handler)){
-            isl.read_data();
-            isl.last_measurement.time_ms = safe_elapsed_ms_c1;
-            guardar_en_buffer(&ISL_handler,ISL_handler.isl_buffer,isl.last_measurement);
-        }
+    //=========================================Leer y guardar ISL29125=========================================
+    if(esta_conectado(&ISL_handler)){
+        isl.read_data();
+        isl.last_measurement.time_ms = safe_elapsed_ms_c1;
+        guardar_en_buffer(&ISL_handler,ISL_handler.isl_buffer,isl.last_measurement);
+    }
+    //=========================================================================================================
 
     return true;
 }
